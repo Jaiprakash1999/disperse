ee0e4ea6991b40b90b755a6bd2614d1e494e1fb6
Jaiprakash kushwaha <jaiprakash.kushwaha@cogoport.com>
2023-09-20 11:55:17 +0530

    priority order of duplicate error is at last

--- src/Disperse/index.js
+++ src/Disperse/index.js
@@ -58,8 +58,8 @@ const Disperse = () => {
 
         const allErrors = [
             ...formatErrors,
-            ...duplicateErrors,
             ...invalidAmountErrors,
+            ...duplicateErrors,
         ];
         setError(allErrors);
 
